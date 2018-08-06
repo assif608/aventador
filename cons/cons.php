9c279f05bcd2369bdfb830ac2de205a9ea75b88d
Assif608 <assifsalam608@gmail.com>
2018-08-06 21:59:10 +0530

    commit1

---
+++ cons/cons.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>VIP constructions</title>
+	<link rel="icon" type="text/css" href="1.jpg">
+	<link rel="stylesheet" type="text/css" href="cons.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div id="qw">
+		<h1>VIP CONSTRUCTIONS</h1>
+	</div>
+	<div id="we">
+		<ul>
+			<li><a href="home.php">Home</a></li>
+			<li><a href="gallery.php">Gallery</a></li>
+			<li><a href="proj.php">Major projects</a></li>
+			<li><a href="about.php">About Us</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+	</div>
+	<div id="bo">
+		<h3>Innovation through us.....</h3>
+		<h2> We make your dream true...</h2>
+	</div>
+</body>
+<footer>
+	<div id="ri">
+		<h3>follow us;</h3>
+		<a href="https://www.facebook.com/"><span class="fa fa-facebook"></a></span>
+		<a href="https://www.instagram.com/"><span class="fa fa-instagram"></a></span>
+		<a href="https://www.twitter.com/"><span class="fa fa-twitter"></a></span>
+</footer>
+</html>
